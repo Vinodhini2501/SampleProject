17c166d456f457247a755a64256a0c282a78b4a3
Vinodhini2501 <vinovj2125@gmail.com>
2021-04-22 05:49:53 +0530

    Adding demo project into master branch

---
+++ src/org/empl/EmployeeDetails.java
@@ -0,0 +1,20 @@
+package org.empl;
+
+public class EmployeeDetails {
+public static void main(String[] args) {
+	int empId= 123;
+	System.out.println("Employee ID:" + empId);
+	String empName="Shiva";
+	System.out.println("Employee Name:" + empName);
+	String empEmail="abc@gmail.com";
+	System.out.println("Employee Email ID:" + empEmail);
+	long empPhone= 8964279201l;
+	System.out.println("Employee Phone Number:" + empPhone);
+	float empSalary= 35206.21f;
+	System.out.println("Employee Salary:" + empSalary);
+	String empGender= "Male";
+	System.out.println("Employee Gender is:" + empGender);
+	String empCity= "Chennai";
+	System.out.println("Employee City:" + empCity);		
+}
+}
